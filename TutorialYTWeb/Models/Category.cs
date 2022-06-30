f4b73715234c27b1adf4bd801fd28dae0b1df51b
ibbra <ibbraba@gmail.com>
2022-06-30 16:14:51 +0200

    Initial commit

---
+++ TutorialYTWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace TutorialYTWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public int DisplayOrder{ get; set; }
+        public DateTime CreateDateTime{ get; set; } = DateTime.Now
+    }
+}
