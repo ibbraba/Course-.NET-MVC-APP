f31c7bc5c3cf54d81766cf6a48afeaa6a63f6f4c
ibbra <ibbraba@gmail.com>
2022-06-30 17:06:45 +0200

    Category queries

---
+++ TutorialYTWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TutorialYTWeb.Data;
+
+namespace TutorialYTWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var ObjCategoryList = _db.Categories.ToList();
+
+
+
+            return View();
+        }
+    }
+}
